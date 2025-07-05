350a8f9df869f68ab87ff29194ea081f32adea95
gabriel <gabriel.donascimento@jala.university>
2025-07-05 00:15:56 -0300

    feat: players const

---
+++ src/const/Player.js
@@ -0,0 +1,43 @@
+export const MARIA = {
+    NAME: 'Maria',
+    SPEED: 4.0,
+    DIRECTION: 3,
+    POWER: 3
+};
+
+export const ABREEL = {
+    NAME: 'Abreel',
+    speed: 3.0,
+    direction: 4,
+    power: 2
+};
+
+export const NEYTHAN = {
+    NAME: 'Neythan',
+    speed: 2.0,
+    direction: 3,
+    power: 3
+};
+
+export const JAMES = {
+    NAME: 'James',
+    speed: 5.0,
+    direction: 2,
+    power: 5
+};
+
+export const LOUIS = {
+    NAME: 'Louis',
+    speed: 3.0,
+    direction: 4,
+    power: 4
+};
+
+export const KONGO = {
+    NAME: 'Kongo',
+    speed: 2.0,
+    direction: 2,
+    power: 5
+};
+
+export const PLAYERS = [MARIA, ABREEL, NEYTHAN, JAMES, LOUIS, KONGO];
